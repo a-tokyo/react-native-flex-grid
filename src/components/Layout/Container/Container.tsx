9d75f50e5e1e283534a765c0a2e6905a915c09ad
Tokyo <ahmed.tokyo1@gmail.com>
2022-12-27 15:50:56 +0000

    enhance

--- src/components/Layout/Container/Container.tsx
+++ src/components/Layout/Container/Container.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, ViewProps } from 'react-native';
+import { Dimensions } from 'react-native';
+
 
 import { getGridBreakpoint, getContainerPaddingHorizontalMap, getContainerMaxWidthsMap } from '../../../utils/responsive';
 
@@ -15,6 +17,7 @@ export declare interface ContainerProps extends ViewProps {
 const styles = StyleSheet.create({
   container: {
     width: '100%',
+    justifySelf: 'center',
   },
   fluid: {
     maxWidth: '100%',
@@ -23,15 +26,21 @@ const styles = StyleSheet.create({
 
 /** Container */
 const Container = ({ style, fluid, Element = View, noPadding, ...rest }: ContainerProps) => {
-  const gridBreakpoint = getGridBreakpoint()
+  /** current grid breakpoint */
+  const gridBreakpoint = getGridBreakpoint();
+  /** container maxWidth */
+  const maxWidth = getContainerMaxWidthsMap[gridBreakpoint];
+  /** screen width */
+  const SCREEN_WIDTH = Dimensions.get('window').width;
+
   return (
     <Element
       style={[
         styles.container,
         noPadding ? null : { paddingHorizontal: getContainerPaddingHorizontalMap()[gridBreakpoint] },
         fluid ? styles.fluid : {
-          ...styles.container,
-          maxWidth: getContainerMaxWidthsMap[gridBreakpoint],
+          maxWidth,
+          marginHorizontal: typeof maxWidth === 'number' ? (SCREEN_WIDTH - maxWidth) / 2 : undefined,
         },
       ]}
       {...rest}
