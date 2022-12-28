4660d7325e291319c0acc714a8d5eb790862c256
Tokyo <ahmed.tokyo1@gmail.com>
2022-12-28 04:09:50 +0000

    enhance docs

--- scripts/prep-story-readme.js
+++ scripts/prep-story-readme.js
@@ -5,17 +5,28 @@ const packageJson = require('../package.json');
 
 const main = () => {
   /** Readme text */
-  const readmeFile = fs.readFileSync(path.join(__dirname, '../README.md'), 'utf8');
-  
+  const readmeFile = fs.readFileSync(
+    path.join(__dirname, '../README.md'),
+    'utf8',
+  );
+
   /** Modified Story file text */
   const storyFileText = `<Meta title="README" />
 
 //This file is auto generated from README.md -- any manual changes will be discarded
 
-${readmeFile.replaceAll(`${packageJson.homepage}/`, '/')}`;
-  
+${readmeFile
+  // for subpaths
+  .replaceAll(`${packageJson.homepage}/`, '/')
+  // for main url
+  .replaceAll(packageJson.homepage, '/')}`;
+
   /** Write story file */
-  fs.writeFileSync(path.join(__dirname, '../src/README.stories.mdx'), storyFileText, 'utf8');
+  fs.writeFileSync(
+    path.join(__dirname, '../src/README.stories.mdx'),
+    storyFileText,
+    'utf8',
+  );
 };
 
 main();
